027a82e1413880437b2d1a5d0934f85740926f19
intanpuspitapuryanti <intanpuspita899@gmail.com>
2025-05-08 21:54:51 +0700

    Create quiz-data.js

---
+++ quiz-data.js
@@ -0,0 +1,38 @@
+// Data kuis dengan gambar dari folder assets
+const questions = [
+    {
+        question: "Hewan apa ini?",
+        image: "assets/images/cat.jpg",
+        imageAlt: "Kucing",
+        options: ["Kucing", "Anjing", "Kelinci", "Hamster"],
+        answer: "Kucing"
+    },
+    {
+        question: "Hewan apa ini?",
+        image: "assets/images/elephant.jpg",
+        imageAlt: "Gajah",
+        options: ["Jerapah", "Gajah", "Singa", "Harimau"],
+        answer: "Gajah"
+    },
+    {
+        question: "Hewan apa ini?",
+        image: "assets/images/monkey.jpg",
+        imageAlt: "Monyet",
+        options: ["Gorila", "Orangutan", "Monyet", "Simpanse"],
+        answer: "Monyet"
+    },
+    {
+        question: "Hewan apa ini?",
+        image: "assets/images/fish.jpg",
+        imageAlt: "Ikan",
+        options: ["Ikan", "Ubur-ubur", "Gurita", "Lumba-lumba"],
+        answer: "Ikan"
+    },
+    {
+        question: "Hewan apa ini?",
+        image: "assets/images/bird.jpg",
+        imageAlt: "Burung",
+        options: ["Ayam", "Bebek", "Burung", "Angsa"],
+        answer: "Burung"
+    }
+];
